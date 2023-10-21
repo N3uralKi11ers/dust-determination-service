d2ea0bd7fbebdd70816f9623ad458bd567be96b7
NikolaySimakov <n.simakoff@gmail.com>
2023-10-21 21:24:23 +0300

    Add: axios

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import axios from './plugins/axios'
+// import VueVideoPlayer from '@videojs-player/vue'
+// import 'video.js/dist/video-js.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// app use
+// app.use(VueVideoPlayer)
+
+app.use(axios, {
+	baseUrl: 'http://0.0.0.0:80',
+})
+
+app.mount('#app')
