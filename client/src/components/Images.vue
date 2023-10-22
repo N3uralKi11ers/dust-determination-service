a8e58aa10349d47deea8de1fb9fb8d6c236021f1
NikolaySimakov <n.simakoff@gmail.com>
2023-10-22 08:37:04 +0300

    Update: images component, add props

--- client/src/components/Images.vue
+++ client/src/components/Images.vue
@@ -1,22 +1,31 @@
 <template>
 	<div>
-		<img :src="getImageBefore()" alt="Image_before" class="inline w-1/2" />
-		<img :src="getImageAfter()" alt="Image_after" class="inline w-1/2" />
+		<img
+			:src="getImageBefore()"
+			alt="Image_before"
+			class="inline w-1/2 pr-4 rounded"
+		/>
+		<img
+			:src="getImageAfter()"
+			alt="Image_after"
+			class="inline w-1/2 pl-4 rounded"
+		/>
 	</div>
 </template>
 <script>
 export default {
+	props: {
+		imageId: Number(),
+	},
 	data() {
-		return {
-			image_id: 1,
-		}
+		return {}
 	},
 	methods: {
 		getImageBefore() {
-			return 'http://0.0.0.0/' + 'images/before/' + this.image_id
+			return 'http://0.0.0.0/' + 'images/before/' + this.imageId
 		},
 		getImageAfter() {
-			return 'http://0.0.0.0/' + 'images/after/' + this.image_id
+			return 'http://0.0.0.0/' + 'images/after/' + this.imageId
 		},
 	},
 }
