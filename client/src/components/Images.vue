ad462c9279326d9f66ae9e2704c9125fe1e3a3fc
NikolaySimakov <n.simakoff@gmail.com>
2023-10-22 03:50:43 +0300

    Init images component

---
+++ client/src/components/Images.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<img :src="getImageBefore()" alt="Image_before" />
+		<img :src="getImageAfter()" alt="Image_after" />
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			image_id: 1,
+		}
+	},
+	methods: {
+		getImageBefore() {
+			return 'http://0.0.0.0/' + 'images/before/' + this.image_id
+		},
+		getImageAfter() {
+			return 'http://0.0.0.0/' + 'images/after/' + this.image_id
+		},
+	},
+}
+</script>
